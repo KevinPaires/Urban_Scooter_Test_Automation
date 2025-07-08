cec24f10e028a96591d3da5992536fcab2a574a3
kevinpaires <kpaires2021@gmail.com>
2025-07-07 23:17:03 -0700

    final commit

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ import data
 
 import helpers
 
+import pages
+
+from selenium import webdriver
+
+
+
 class TestUrbanRoutes:
     @classmethod
     def setup_class(cls):
+        # do not modify - we need additional logging enabled in order to retrieve phone confirmation code
+        from selenium.webdriver import DesiredCapabilities
+        capabilities = DesiredCapabilities.CHROME
+        capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
+        cls.driver = webdriver.Chrome()
+
         if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
             print('Connected to the Urban Routes server')
         else:
@@ -12,43 +24,79 @@ class TestUrbanRoutes:
 
 
     def test_set_route(self):
-        # Add in S8
-        print('function created for set route')
-        pass
+       self.driver.get(data.URBAN_ROUTES_URL)
+       urban_routes_page = pages.UrbanRoutesPage(self.driver)
+       urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
+
+       actual_value_from = urban_routes_page.get_value_in_from()
+       expected_value_from = data.ADDRESS_FROM
+
+       actual_value_to = urban_routes_page.get_value_in_to()
+       expected_value_to = data.ADDRESS_TO
+
+       assert actual_value_from in expected_value_from, f'Expected: {expected_value_from} Actual: {actual_value_from}'
+       assert actual_value_to in expected_value_to, f'Expected: {expected_value_to} Actual: {actual_value_to}'
 
     def  test_select_plan(self):
-        # Add in S8
-        print('function created for set route')
-        pass
+        self.driver.get(data.URBAN_ROUTES_URL)
+        urban_routes_page = pages.UrbanRoutesPage(self.driver)
+        urban_routes_page.select_supportive_plan(data.ADDRESS_FROM, data.ADDRESS_TO)
+
+        active_element = urban_routes_page.verify_supportive_text()
+        assert "Supportive" in active_element, f'Expected: "Supportive" Actual: {active_element}'
+
 
     def test_fill_phone_number(self):
 
-        # Add in S8
-        print('function created for set route')
-        pass
+        self.driver.get(data.URBAN_ROUTES_URL)
+        urban_routes_page = pages.UrbanRoutesPage(self.driver)
+        urban_routes_page.fill_out_phone_number(data.ADDRESS_FROM, data.ADDRESS_TO, data.PHONE_NUMBER)
+
+        actual_number = urban_routes_page.get_phone_number()
+        expected_number = data.PHONE_NUMBER
+        assert expected_number in actual_number, f'Expected: {expected_number} Actual: {actual_number}'
 
     def test_fill_card(self):
-        # Add in S8
-        print('function created for set route')
-        pass
+        self.driver.get(data.URBAN_ROUTES_URL)
+        urban_routes_page = pages.UrbanRoutesPage(self.driver)
+        urban_routes_page.fill_out_credit_card_number(data.ADDRESS_FROM, data.ADDRESS_TO, data.CARD_NUMBER, data.CARD_CODE)
+
+        actual_text = urban_routes_page.click_card_text()
+        expected_text = "Card"
+        assert expected_text in actual_text, f'Expected: {expected_text} Actual: {actual_text}'
 
     def test_comment_for_driver(self):
-        # Add in S8
-        print('function created for set route')
-        pass
+        self.driver.get(data.URBAN_ROUTES_URL)
+        urban_routes_page = pages.UrbanRoutesPage(self.driver)
+        urban_routes_page.write_message_for_driver(data.ADDRESS_FROM, data.ADDRESS_TO, data.MESSAGE_FOR_DRIVER)
+
+        actual_text = urban_routes_page.check_message_text()
+        expected_text = data.MESSAGE_FOR_DRIVER
+        assert actual_text in expected_text, f'Expected: {expected_text} Actual: {actual_text}'
+
 
     def test_order_blanket_and_handkerchiefs(self):
-        # Add in S8
-        print('function created for set route')
-        pass
+        self.driver.get(data.URBAN_ROUTES_URL)
+        urban_routes_page = pages.UrbanRoutesPage(self.driver)
+        urban_routes_page.check_blanket_and_handkerchiefs(data.ADDRESS_FROM, data.ADDRESS_TO)
+
+        assert urban_routes_page.is_slider_on(), f'Slider not enabled'
 
     def test_order_2_ice_creams(self):
-        # Add in S8
-        for i in range(2):
-            print('function created for set route')
-        pass
+        self.driver.get(data.URBAN_ROUTES_URL)
+        urban_routes_page = pages.UrbanRoutesPage(self.driver)
+        urban_routes_page.add_ice_cream(data.ADDRESS_FROM, data.ADDRESS_TO)
+
+        actual_count = urban_routes_page.check_ice_cream_count()
+        assert "2" in actual_count, f'Expected: {actual_count} Actual: {actual_count}'
 
     def test_car_search_model_appears(self):
-        # Add in S8
-        print('function created for set route')
-        pass
+        self.driver.get(data.URBAN_ROUTES_URL)
+        urban_routes_page = pages.UrbanRoutesPage(self.driver)
+        urban_routes_page.verify_modal_display(data.ADDRESS_FROM, data.ADDRESS_TO, data.PHONE_NUMBER, data.MESSAGE_FOR_DRIVER)
+
+        assert urban_routes_page.modal_is_displayed(), f'Modal not displayed'
+
+    @classmethod
+    def teardown_class(cls):
+        cls.driver.quit()
---
+++ pages.py
@@ -0,0 +1,285 @@
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+import helpers
+
+class UrbanRoutesPage:
+    # supportive option locators
+    FROM_LOCATOR = (By.ID, 'from')
+    TO_LOCATOR = (By.ID, 'to')
+    CUSTOM_OPTION_LOCATOR = (By.XPATH, '//div[text()="Custom"]')
+    LOCATE_TAXI_ICON =  (By.XPATH, '//img[contains(@src, "taxi-active") and contains(@class, "type-icon")]')
+    LOCATE_TAXI_BUTTON = (By.XPATH, '//button[@class="button round"]' )
+    LOCATE_SUPPORTIVE_ICON = (By.XPATH, '//img[@alt="Supportive"]')
+    # Phone number locators
+    LOCATE_PHONE_NUMBER = (By.XPATH, '//div[@class="np-text" and text()="Phone number"]')
+    LOCATE_ENTER_PHONE_NUMBER = (By.ID, 'phone')
+    LOCATE_PHONE_NUMBER_BUTTON = (By.XPATH, '//button[@class="button full"]')
+    PHONE_CODE = (By.ID, 'code')
+    ENTER_CARD_CODE = (By.XPATH, '//label[text()="Enter the code]')
+    CONFIRM_PHONE_CODE = (By.XPATH, '//button[@type="submit" and normalize-space()="Confirm"]')
+    SUPPORTIVE_TEXT_LOCATOR = (By.CSS_SELECTOR, '.tcard.active')
+    # CC LOCATORS
+    LOCATE_PAYMENT_METHOD = (By.XPATH, '//div[@class="pp-button filled"]')
+    ADD_CREDIT_CARD = (By.XPATH, '//img[contains(@src, "plus") and contains(@class, "pp-plus")]')
+    ENTER_CREDIT_CARD = (By.ID, "number")
+    TYPE_CREDIT_CARD_CODE = (By.NAME, 'code')
+    LOCATE_LINK_BUTTON = (By.XPATH, '//button[normalize-space()="Link"]')
+    LOCATE_CARD_TEXT = (By.XPATH, '//div[@class="pp-value-text"]')
+    DRIVER_MESSAGE = (By.ID, 'comment')
+    # slider locators
+    LOCATE_SLIDER = (By.XPATH, '//span[@class="slider round"]')
+    LOCATE_CHECKBOX = (By.XPATH, '//input[@class="switch-input"]')
+    # ice cream locators
+    LOCATE_ADD_ICE_CREAM = (By.CLASS_NAME, 'counter-plus')
+    LOCATE_ICE_CREAM_COUNT = (By.CLASS_NAME, 'counter-value')
+    # modal locators
+    LOCATE_ORDER_BUTTON = (By.XPATH, '//button[@class="smart-button"]')
+    LOCATE_MODAL = (By.XPATH, '//div[@class="order-subbody"]')
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def enter_from_location(self, from_text):
+        self.driver.find_element(*self.FROM_LOCATOR).send_keys(from_text)
+
+    def enter_to_location(self, to_text):
+        self.driver.find_element(*self.TO_LOCATOR).send_keys(to_text)
+
+    def enter_locations(self, from_text, to_text):
+        self.enter_from_location(from_text)
+        self.enter_to_location(to_text)
+
+    def get_value_in_from(self):
+        return self.driver.find_element(*self.FROM_LOCATOR).get_attribute("value")
+
+    def get_value_in_to(self):
+        return self.driver.find_element(*self.TO_LOCATOR).get_attribute("value")
+
+    def click_custom_option_button(self):
+        self.driver.find_element(*self.CUSTOM_OPTION_LOCATOR).click()
+
+    def click_taxi_icon(self):
+        self.driver.find_element(*self.LOCATE_TAXI_ICON).click()
+
+    def click_taxi_button(self):
+        self.driver.find_element(*self.LOCATE_TAXI_BUTTON).click()
+
+    def click_supportive_icon(self):
+        self.driver.find_element(*self.LOCATE_SUPPORTIVE_ICON).click()
+
+    def verify_supportive_text(self):
+        return self.driver.find_element(*self.SUPPORTIVE_TEXT_LOCATOR).text
+
+    def click_enter_phone_number(self):
+        self.driver.find_element(*self.LOCATE_PHONE_NUMBER).click()
+
+    def enter_phone_number(self, phone_number):
+        self.driver.find_element(*self.LOCATE_ENTER_PHONE_NUMBER).send_keys(phone_number)
+
+    def click_add_phone_number(self):
+        self.driver.find_element(*self.LOCATE_PHONE_NUMBER_BUTTON).click()
+
+    def click_phone_code(self, code):
+        self.driver.find_element(*self.PHONE_CODE).send_keys(code)
+
+    def get_phone_number(self):
+        return self.driver.find_element(*self.LOCATE_ENTER_PHONE_NUMBER).get_attribute("value")
+
+    def enter_code_in_code_field(self):
+        self.driver.find_element(*self.ENTER_CARD_CODE).click()
+
+    def click_payment_method(self):
+        self.driver.find_element(*self.LOCATE_PAYMENT_METHOD).click()
+
+    def click_add_credit_card(self):
+        self.driver.find_element(*self.ADD_CREDIT_CARD).click()
+
+    def click_enter_credit_card(self):
+        self.driver.find_element(*self.ENTER_CREDIT_CARD).click()
+
+    def enter_credit_card(self, credit_card):
+        field = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.ENTER_CREDIT_CARD)
+        )
+        field.clear()
+        field.send_keys(credit_card)
+
+    def enter_credit_card_code(self, code):
+        cvv_field = WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable(self.TYPE_CREDIT_CARD_CODE)
+        )
+        cvv_field.clear()
+        cvv_field.send_keys(code)
+        cvv_field.send_keys(Keys.TAB)
+
+
+    def click_link_button(self):
+        button = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.LOCATE_LINK_BUTTON)
+        )
+        button.click()
+    def click_card_text(self):
+        return self.driver.find_element(*self.LOCATE_CARD_TEXT).text
+
+    def comment_for_driver(self, message):
+        self.driver.find_element(*self.DRIVER_MESSAGE).send_keys(message)
+
+    def check_message_text(self):
+        return self.driver.find_element(*self.DRIVER_MESSAGE).text
+
+    def click_slider(self):
+        slider = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.LOCATE_SLIDER)
+        )
+        slider.click()
+
+    def is_slider_on(self):
+
+        checkbox = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(self.LOCATE_CHECKBOX)
+        )
+        return checkbox.get_property("checked")
+
+    def click_add_ice_cream(self):
+        for i in range (2):
+            self.driver.find_element(*self.LOCATE_ADD_ICE_CREAM).click()
+
+
+    def check_ice_cream_count(self):
+        return self.driver.find_element(*self.LOCATE_ICE_CREAM_COUNT).text
+
+    def click_order_button(self):
+        self.driver.find_element(*self.LOCATE_ORDER_BUTTON).click()
+
+    def modal_is_displayed(self):
+        modal = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(self.LOCATE_MODAL)
+        )
+        return modal.is_displayed()
+
+    def click_confirm_code(self):
+        modal = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(self.CONFIRM_PHONE_CODE)
+        )
+        modal.click()
+
+
+
+    def select_supportive_plan(self, from_text, to_text):
+        self.driver.implicitly_wait(5)
+        self.enter_from_location(from_text)
+        self.enter_to_location(to_text)
+        self.click_custom_option_button()
+        self.click_taxi_icon()
+        self.click_taxi_button()
+        self.click_supportive_icon()
+        time.sleep(5)
+
+    def fill_out_phone_number(self, from_text, to_text, phone_number):
+        self.driver.implicitly_wait(5)
+        self.enter_from_location(from_text)
+        self.enter_to_location(to_text)
+        self.click_custom_option_button()
+        self.click_taxi_icon()
+        self.click_taxi_button()
+        self.click_supportive_icon()
+        self.click_enter_phone_number()
+        self.enter_phone_number(phone_number)
+        self.click_add_phone_number()
+        code = helpers.retrieve_phone_code(self.driver)
+        self.click_phone_code(code)
+        time.sleep(5)
+        self.click_confirm_code()
+
+    def fill_out_credit_card_number(self, from_text, to_text, credit_card, code):
+        self.driver.implicitly_wait(5)
+        self.enter_from_location(from_text)
+        self.enter_to_location(to_text)
+        self.click_custom_option_button()
+        self.click_taxi_icon()
+        self.click_taxi_button()
+        self.click_supportive_icon()
+        self.click_payment_method()
+        time.sleep(5)
+        self.click_add_credit_card()
+        time.sleep(10)
+        self.enter_credit_card(credit_card)
+        self.enter_credit_card_code(code)
+        self.click_link_button()
+        time.sleep(5)
+
+
+    def write_message_for_driver(self, from_text, to_text, message):
+        self.driver.implicitly_wait(5)
+        self.enter_from_location(from_text)
+        self.enter_to_location(to_text)
+        self.click_custom_option_button()
+        self.click_taxi_icon()
+        self.click_taxi_button()
+        self.click_supportive_icon()
+        time.sleep(5)
+        self.comment_for_driver(message)
+
+    def check_blanket_and_handkerchiefs(self, from_text, to_text):
+        self.driver.implicitly_wait(5)
+        self.enter_from_location(from_text)
+        self.enter_to_location(to_text)
+        self.click_custom_option_button()
+        self.click_taxi_icon()
+        self.click_taxi_button()
+        self.click_supportive_icon()
+        self.click_slider()
+        time.sleep(5)
+
+    def add_ice_cream(self,from_text, to_text):
+        self.driver.implicitly_wait(5)
+        self.enter_from_location(from_text)
+        self.enter_to_location(to_text)
+        self.click_custom_option_button()
+        self.click_taxi_icon()
+        self.click_taxi_button()
+        self.click_supportive_icon()
+        self.click_add_ice_cream()
+        time.sleep(5)
+
+    def verify_modal_display(self, from_text, to_text, phone_number, message):
+        self.driver.implicitly_wait(5)
+        self.enter_from_location(from_text)
+        self.enter_to_location(to_text)
+        self.click_custom_option_button()
+        self.click_taxi_icon()
+        self.click_taxi_button()
+        self.click_supportive_icon()
+        self.click_enter_phone_number()
+        self.enter_phone_number(phone_number)
+        self.click_add_phone_number()
+        code = helpers.retrieve_phone_code(self.driver)
+        self.click_phone_code(code)
+        time.sleep(5)
+        self.click_confirm_code()
+        time.sleep(5)
+        self.comment_for_driver(message)
+        self.click_order_button()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
